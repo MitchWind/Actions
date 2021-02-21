07a53b56d42ba8ed04fd6ea005533dc91e43bfd5
wind <mofeng64@gmail.com>
2021-02-22 04:57:12 +0800

    为什么要这样对待我

--- openwrt/x86/confingbefore.sh
+++ openwrt/x86/confingbefore.sh
@@ -1,5 +1,3 @@
-echo $(pwd)
-ls
 wget -P tools/upx/ https://raw.githubusercontent.com/streetwind/openwrt-tools/main/upx/Makefile
 wget -P tools/ucl/ https://raw.githubusercontent.com/coolsnowwolf/lede/master/tools/ucl/Makefile
 sed -i "38i tools-y += ucl upx" tools/Makefile
