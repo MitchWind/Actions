84bceef5d8616483e289ef92c3fcd3657abe8fb2
wind <mofeng64@gmail.com>
2021-02-22 01:14:16 +0800

    update

--- openwrt/x86/confingbefore.sh
+++ openwrt/x86/confingbefore.sh
@@ -1,3 +1,5 @@
+pwd
+ls -lah
 wget -P tools/upx/ https://raw.githubusercontent.com/streetwind/openwrt-tools/main/upx/Makefile
 wget -P tools/ucl/ https://raw.githubusercontent.com/coolsnowwolf/lede/master/tools/ucl/Makefile
 sed -i "38i tools-y += ucl upx" tools/Makefile
