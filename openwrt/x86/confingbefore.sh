422bd21f8b8f28f3fc15e33f763f878a5e6c543b
wind <mofeng64@gmail.com>
2021-02-22 02:57:40 +0800

    修复shell

--- openwrt/x86/confingbefore.sh
+++ openwrt/x86/confingbefore.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 echo $(pwd)
 wget -P tools/upx/ https://raw.githubusercontent.com/streetwind/openwrt-tools/main/upx/Makefile
 wget -P tools/ucl/ https://raw.githubusercontent.com/coolsnowwolf/lede/master/tools/ucl/Makefile
