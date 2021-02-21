ddb60c8cf2e8e3c33edfba7856ef7c1758e32667
wind <mofeng64@gmail.com>
2021-02-22 01:26:46 +0800

    错误修正

--- openwrt/x86/confingbefore.sh
+++ openwrt/x86/confingbefore.sh
@@ -1,4 +1,4 @@
-pwd
+echo $(pwd)
 ls -lah
 wget -P tools/upx/ https://raw.githubusercontent.com/streetwind/openwrt-tools/main/upx/Makefile
 wget -P tools/ucl/ https://raw.githubusercontent.com/coolsnowwolf/lede/master/tools/ucl/Makefile
