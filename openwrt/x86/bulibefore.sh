b17a1cce0e690083ee84105c389d331a0e8dba4d
wind <mofeng64@gmail.com>
2021-02-22 01:43:23 +0800

    修复脚本

--- openwrt/x86/bulibefore.sh
+++ openwrt/x86/bulibefore.sh
@@ -3,6 +3,6 @@ sed -i 's/255.255.255.0/255.255.0.0/g' package/base-files/files/bin/config_gener
 sed -i 's/OpenWrt/Wind/g' package/base-files/files/bin/config_generate
 sed -i 's/+ariang/+ariang-nginx/g' package/feeds/luci/luci-app-aria2/Makefile
 sed -i '/DISTRIB_REVISION/d' package/base-files/files/etc/openwrt_release
-echo "DISTRIB_REVISION='$(date '+%Y.%m.%d')'" >> package/base-files/files/etc/openwrt_release
+echo "DISTRIB_REVISION='$(date '+%Y.%m.%d')'" >>package/base-files/files/etc/openwrt_release
 sed -i '/DISTRIB_DESCRIPTION/d' package/base-files/files/etc/openwrt_release
-echo "DISTRIB_DESCRIPTION='Wind '" >> package/base-files/files/etc/openwrt_release
+echo "DISTRIB_DESCRIPTION='Wind '" >>package/base-files/files/etc/openwrt_release
