e7967b3331421c140ea1fd232cfab0a5f9240c5d
wind <mofeng64@gmail.com>
2021-02-22 08:39:52 +0800

    count

--- openwrt/x86/comply.sh
+++ openwrt/x86/comply.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 sed -i 's/255.255.255.0/255.255.0.0/g' package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/Wind/g' package/base-files/files/bin/config_generate
--- openwrt/x86/ready.sh
+++ openwrt/x86/ready.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.
 wget -P tools/upx/ https://raw.githubusercontent.com/streetwind/openwrt-tools/main/upx/Makefile
 wget -P tools/ucl/ https://raw.githubusercontent.com/coolsnowwolf/lede/master/tools/ucl/Makefile
 sed -i "38i tools-y += ucl upx" tools/Makefile
