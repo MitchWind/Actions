c635f4b45697f74b1351227c4a34f8c195e6ced7
wind <mofeng64@gmail.com>
2021-03-04 13:55:19 +0800

    替换FTP

--- openwrt/x86/comply.sh
+++ openwrt/x86/comply.sh
@@ -3,6 +3,7 @@ sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generat
 sed -i 's/255.255.255.0/255.255.0.0/g' package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/Wind/g' package/base-files/files/bin/config_generate
 sed -i 's/+ariang/+ariang-nginx/g' package/feeds/luci/luci-app-aria2/Makefile
+sed -i 's/wget-ssl/wget/g' package/feeds/wind/luci-app-jd-dailybonus/root/usr/share/jd-dailybonus/newapp.sh
 sed -i '/DISTRIB_REVISION/d' package/base-files/files/etc/openwrt_release
 echo "DISTRIB_REVISION='$(date '+%Y.%m.%d')'" >>package/base-files/files/etc/openwrt_release
 sed -i '/DISTRIB_DESCRIPTION/d' package/base-files/files/etc/openwrt_release
