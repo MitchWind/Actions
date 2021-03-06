1f5fc5565e987c96ed5d7e8d76fe40b42edfcef7
wind <mofeng64@gmail.com>
2021-03-06 12:09:51 +0800

    修改

--- lienol/x86/comply.sh
+++ lienol/x86/comply.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -i '/root/d' package/lean/default-settings/files/zzz-default-settings
-sed -i 's/OpenWrt/RodeWind/g' package/lean/default-settings/files/zzz-default-settings
+sed -i '/root/d' package/default-settings/files/zzz-default-settings
+sed -i 's/OpenWrt/RodeWind/g' package/default-settings/files/zzz-default-settings
